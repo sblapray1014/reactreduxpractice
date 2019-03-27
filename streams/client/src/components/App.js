9e10933602472fd8ba8a88f16c259dc4269bd777
Spencer LaPray <sblapray@yahoo.com>
2019-03-26 22:57:37 -0600

    adding stream udemy app

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/streams/new" component={StreamCreate} />
+          <Route exact path="/streams/edit" component={StreamEdit} />
+          <Route exact path="/streams/delete" component={StreamDelete} />
+          <Route exact path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
